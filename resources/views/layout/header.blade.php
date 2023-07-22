4ae5c5e0988758761611bfc035f765ace19e70f1
vijay kumar <308vijaykumar@gmail.com>
2023-07-22 19:50:53 +0530

    user profile dropdown menu

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -10,10 +10,19 @@
             <li><a href="{{route('feeds')}}" class="text-white {{ request()->is('feeds') ? "active" :" "}}">Feeds</a></li>
             <li><a href="{{url('/notes')}}" class="text-white">Notes</a></li>
         </ul>
-    </nav>
+    </nav>  
     <div class="Registrations">
         @auth
-        <a href="{{route('logout')}}" class="text-white">Logout</a>
+        <div class="dropdown">
+            <i class="fa-solid fa-circle-user" id="userAccount"></i>
+            <ul class="user_profile dropdown-content" id="userProfile">
+                <li>Hi, {{Auth::user()->username}}</li>
+                <a href="{{url('users/profile')}}"><li>View Profile</li></a>
+                <a href="{{ asset('users/changePassword')}}"><li>change password</li></a>
+                <a href="{{route('logout')}}"><li>Logout</li></a>
+            </ul>
+        </div>
+            
         @else
         <a href="{{route('login')}}" class="btn btn-primary">Login</a>
         <a href="{{route('singup')}}" class="btn btn-danger">Register</a>
