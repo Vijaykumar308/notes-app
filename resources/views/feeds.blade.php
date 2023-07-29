2da1a0f76cfc44ecc5e45d1b4f6f54f23092883c
vijay kumar <308vijaykumar@gmail.com>
2023-07-29 19:16:35 +0530

    feed post is also complete

--- resources/views/feeds.blade.php
+++ resources/views/feeds.blade.php
@@ -33,7 +33,62 @@
             </li>
         </ul>
     </div>
-    <div class="item-2">2</div>
+    <div class="item-2">
+        @for ($i = 1; $i < 5; $i++)
+            <div class="posts-container">
+                <div class="posts-topbar">
+                    <div class="profile">
+                        <figure class="profile-pic">
+                            <img src="{{asset('images/example/profile-pic.jpg')}}" alt="profile-pic">
+                        </figure>
+
+                        <div class="profile-block">
+                            <a href="users/messages/mr.vijay.kumar">
+                                <h1 class="messages-block username">                           
+                                    mr.vijay.kumar
+                                </h1>
+                                <p class="">23 hours ago</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="topbar-right-side">
+                        <button class="save-btn-icon reaction-counts remove-btn-default-style">
+                            <i class="fa-regular fa-bookmark"></i>
+                            {{-- <i class="fa-solid fa-bookmark"></i> --}}
+                        </button>
+                    </div>
+                </div>
+
+                <div class="posts-content">
+                    <h3 class="post-heading"> My first Blog</h3>
+                    <p class="post-message"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et exercitationem quas aliquid illo rerum, in consectetur ratione quaerat quo placeat cumque hic fugit unde temporibus praesentium porro quae aliquam aspernatur? Pariatur, libero quis. Ut iste itaque rem quis ab laudantium rerum deserunt at animi nostrum numquam veritatis mollitia incidunt, a facilis ad voluptates debitis aspernatur optio autem soluta est maxime modi repellendus? Suscipit eos officiis quos ratione, quisquam commodi esse mollitia veritatis harum atque fugit iste non corrupti qui, voluptatum reprehenderit nemo. Veritatis vero, earum debitis voluptas enim illo nam animi est quia quaerat cumque consequuntur ullam natus dignis. </p>
+                </div>
+
+                <div class="posts-footer">
+                    <div class="posts-footer-items">
+                        <button class="remove-btn-default-style">
+                            <i class="fa-regular fa-heart reaction-counts"></i>
+                            {{-- <i class="fa-solid fa-heart"></i> --}}
+                            <span class="reaction-counts">10k</span>
+                        </button>
+                        <button class="remove-btn-default-style">
+                            <i class="fa-regular fa-thumbs-down reaction-counts"></i>
+                            {{-- <i class="fa-solid fa-heart-crack"></i> --}}
+                            <span class="reaction-counts">4k</span>
+                        </button>
+                        <button class="remove-btn-default-style">
+                            <i class="fa-solid fa-share-nodes reaction-counts"></i>
+                            <span class="reaction-counts"> 3k </span>
+                        </button>
+                    </div>
+                    <button class="remove-btn-default-style">
+                        <i class="fa-solid fa-eye"></i>
+                        <span class="reaction-counts"> 20k</span>
+                    </button>
+                </div>
+            </div>  
+        @endfor
+    </div>
 
     <div class="item-3">
         <div class="messages-menu item-1">
